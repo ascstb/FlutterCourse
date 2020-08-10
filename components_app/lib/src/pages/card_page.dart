ee07f6d1b091fc2ac8132c451bbab559d99d2895
Arturo Sanchez <r2@r2devpros.com>
2020-08-10 02:00:41 -0500

    Section 6 - Lesson 75: Customizing Card Styles

--- components_app/lib/src/pages/card_page.dart
+++ components_app/lib/src/pages/card_page.dart
@@ -23,6 +23,9 @@ class CardPage extends StatelessWidget {
   Widget _cardType1() {
     print("CardPage_TAG: _cardType1: ");
     return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
       child: Column(
         children: [
           ListTile(
@@ -54,15 +57,14 @@ class CardPage extends StatelessWidget {
 
   Widget _cardType2() {
     print("CardPage_TAG: _cardType2: ");
-    return Card(
+    final card = Container(
       child: Column(
         children: [
           FadeInImage(
             placeholder: AssetImage("assets/jar_loading.gif"),
-            image: NetworkImage("https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg"),
-            fadeInDuration: Duration(
-              milliseconds: 200
-            ),
+            image: NetworkImage(
+                "https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg"),
+            fadeInDuration: Duration(milliseconds: 200),
             height: 250.0,
             fit: BoxFit.cover,
           ),
@@ -71,6 +73,25 @@ class CardPage extends StatelessWidget {
             child: Text("Image about something"),
           )
         ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow> [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
