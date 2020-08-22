851aba9df8acf40ccd93ed656ef51920f9009cd4
ascstb <ascstb@gmail.com>
2020-08-22 18:20:11 -0500

    Form Validation: Section11 - Lesson 206: Keeping data on hot reload

--- form_validation/lib/src/blocs/provider.dart
+++ form_validation/lib/src/blocs/provider.dart
@@ -3,9 +3,20 @@ import 'package:form_validation/src/blocs/login_block.dart';
 export 'package:form_validation/src/blocs/login_block.dart';
 
 class Provider extends InheritedWidget {
+  static Provider _instance;
+
+  factory Provider({Key key, Widget child}) {
+    if(_instance == null) {
+      _instance = Provider._internal(key: key, child: child);
+    }
+
+    return _instance;
+  }
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  //Provider({Key key, Widget child}) : super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
